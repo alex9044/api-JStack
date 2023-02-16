559407e0c199253c2c0e475d16d29c669bfdcb99
Alex Moscato <alexmoscato9044@gmail.com>
2023-02-15 22:33:57 -0300

    Adicionado o mongoose e o nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => { console.log('Conectado ao mongo');})
+    .catch(() => { console.log('Erro ao conectar ao mongo');});
 
 // Primero paramentro Port: 3001, segundo
 app.listen(3001, () => {
